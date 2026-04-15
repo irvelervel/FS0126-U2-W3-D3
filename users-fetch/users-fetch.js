c35061936b0df3618ae1435049a33c6f71346d3b
Stefano Casasola <ste.casasola@gmail.com>
2026-04-15 12:13:30 +0200

    Finishes the dynamic users list

--- users-fetch/users-fetch.js
+++ users-fetch/users-fetch.js
@@ -23,6 +23,50 @@ const getUsers = function () {
     //     // Authorization: 'xxxxxxxxxxxx' // è una chiave per accedere ad un API, non serve con JSON PLACEHOLDER
     // }
   })
+    .then((response) => {
+      // qui definisco il finale POSITIVO della Promise
+      // qui finisco se la Promise è RESOLVED, FULFILLED
+      console.log('RESPONSE', response)
+      // dentro response troverete tante utili informazioni, come lo status code (200 se è tutto ok), la proprietà "ok" che con un semplice booleano ci restituisce l'esito della Promise etc.
+      // quello che però questo oggetto response NON contiene è il JSON della risposta :(
+      // niente paura però! l'estrazione del JSON da questa response si può fare comodamente con un metodo
+      // che si chiama response.json(). Il problema è questo metodo A SUA VOLTA è asincrono e torna una Promise 🫠
+
+      //   quindi come si procede per la seconda? attenzione! il JSON sarà disponibile in questa response solamente
+      // se la response ci ha tornato 200, o se più in generale la proprietà "ok" è TRUE!
+      if (response.ok) {
+        // proseguite a estrarre il JSON!
+        // per essere sicuri che CI SIA un JSON da estrarre devo essere sicuro che la mia chiamata sia andata buon
+        // fine, ovvero che la sua proprietà response.ok sia TRUE
+        return response.json() // dovete ritornarlo, così come fetch ritorna una Promise!
+      } else {
+        // qui dentro ci finireste se siete riusciti a contattare il server ma non avete ottenuto una risposta ok
+        // qui dovremmo gestire l'errore!
+        // una mossa furba potrebbe essere quella di RICICLARE il blocco catch e di teletrasportarci nel blocco catch
+        // come faccio da qui a teletrasportarmi nel blocco catch e gestire in un'UNICA volta l'errore?
+        // sollevando a mano un errore!
+        throw new Error('Risposta ricevuta, ma errore!', response.status)
+      }
+    })
+    .then((data) => {
+      // nel caso del doppio then, in cui il secondo aspetta l'esito di response.json() quello che ottenete
+      // come parametro è il JSON già "parsato"!
+      console.log('ARRAY DI UTENTI', data)
+      // riferimento lista
+      const ul = document.getElementById('users')
+      data.forEach((utente) => {
+        const li = document.createElement('li')
+        li.classList.add('list-group-item')
+        li.innerText = utente.name // prendo solo il nome
+        ul.appendChild(li)
+      })
+    })
+    .catch((error) => {
+      // qui definisco il finale NEGATIVO della Promise
+      // qui finisco se la Promise è REJECTED
+      console.log('ERROR', error)
+      // qui finite quando la vostra Promise NON RIESCE A CONTATTARE IL SERVER
+    })
 }
 
 // avviamo getUsers all'avvio della pagina
